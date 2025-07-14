b17bceeaab14761cf76190d3a01a8255a2c18fbf
skndream2023@gmail.com <skndream2023@gmail.com>
2025-07-14 14:17:46 +0200

    frontend and backend succesfull connection

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.api import xero_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(xero_routes.router)
+
+# Enable CORS in fastapi
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:3000"],  # frontend origin
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
@@ -8,17 +7,48 @@ import axios from 'axios';
 function App() {
   useEffect(() => {
   const fetchData = async () => {
-    const res = await axios.get("http://localhost/", {withCredentials: true});
+    try {
+      const res = await axios.get("http://localhost/", {withCredentials: true});
+      console.log("Backend response: ",res.data)
+    } catch (error) {
+      console.error("Error connecting to backend: ", error);
+    }
+    
   };
+
   fetchData();
 }, []);
 
+const handleConnect = async () => {
+  try {
+    const response = await fetch("http://localhost:8000/login", {
+      method: "GET",
+      credentials: "include" //Needed to keep session cookie
+    });
+
+    // redirect to xero api
+    if (response.redirected) {
+      window.location.href = response.url
+    }
+  } catch (err) {
+    console.error("Failed to connect to Xero", err);
+  }
+};
+
   return (
     <Router>
-      <Routes>
+      <div className="App">
+        <button 
+        onClick={handleConnect}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+           Connect To Xero
+        </button>
+        <Routes>
         <Route path="/" element={<Home />} />
         
       </Routes>
+      </div>
     </Router>
   );
 }
