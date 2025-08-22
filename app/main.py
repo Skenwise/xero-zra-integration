74cadbe411ec9d067a4fe603c0921be120c7d3d0
skndream2023@gmail.com <skndream2023@gmail.com>
2025-08-22 10:17:55 +0200

    #

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3000"],  # frontend origin
+    allow_origins = origins,  # frontend origin
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
