89615580b0f8346d3a607903565e41db6ab0b6c4
skndream2023@gmail.com <skndream2023@gmail.com>
2025-08-22 10:06:34 +0200

    added vercel server to CORS origin

--- app/main.py
+++ app/main.py
@@ -6,6 +6,12 @@ app = FastAPI()
 app.include_router(xero_routes.router)
 
 # Enable CORS in fastapi
+
+origins = [
+    "https://xero-zra-integration.vercel.app",
+    "http://localhost:3000"
+]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins = ["http://localhost:3000"],  # frontend origin
